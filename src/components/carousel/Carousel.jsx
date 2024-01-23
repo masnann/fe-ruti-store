aacb26b8dc5ca03ac954826487f9af8563fdd6e0
Anan Widianto <anan.widi1@gmail.com>
2024-01-23 20:34:13 +0700

    improvement: api homepage

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -9,7 +9,7 @@ const Carousel = ({ data }) => {
       dots={true}
       infinite={true}
       autoplay={true}
-      autoplaySpeed={3000}
+      autoplaySpeed={2000}
       speed={500}
       slidesToShow={1}
       slidesToScroll={1}
